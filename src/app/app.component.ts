26765cfbbddadc9afa8a56df25ab42c6cfec7109
MansourTA <mohamedmansour.taleb@esprit.tn>
2025-03-04 03:23:28 +0100

    Update Collab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {Router, RouterLink, RouterOutlet} from '@angular/router';
 
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
