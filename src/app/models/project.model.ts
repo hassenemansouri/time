3370add8db122b69c36f2be3537316882bf09573
mohamedwassimbahria <mohamedwassim.bahrialasgher@esprit.tn>
2025-03-24 02:29:00 +0000

    done

--- src/app/models/project.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Project {
-  id: string;
-  title: string;
-  description: string;
-  startDate?: Date | null; // Permet null ou undefined
-  endDate?: Date | null;
-  status: 'to-do' | 'in-progress' | 'completed';
-}
