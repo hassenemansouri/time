39e86855b82d3576e2b525b9add2dea99f90fefa
Houssem-Ellouze <houssem.ellouze@esprit.tn>
2025-04-05 16:12:17 +0100

    Implement A Lottie in Goal Component

--- src/app/goal/list-goals.component.ts
+++ src/app/goal/list-goals.component.ts
@@ -1,24 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, CUSTOM_ELEMENTS_SCHEMA, OnInit} from '@angular/core';
 import {Router, RouterLink, RouterLinkActive} from '@angular/router';
 import { GoalService } from './goal.service';
 import { Goal } from './goal.model';
-import { NgForOf } from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-list-goals',
   templateUrl: './list-goals.component.html',
   styleUrls: ['./list-goals.component.css'],
   standalone: true,
-  imports: [NgForOf, RouterLink, RouterLinkActive]
+  imports: [NgForOf, RouterLink, RouterLinkActive, NgIf],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ListGoalsComponent implements OnInit {
   goals: Goal[] = [];
 
   constructor(private goalService: GoalService, private router: Router) {}
 
+  showAnimation = true;  // To control if the animation is visible
+
   ngOnInit(): void {
-    this.loadGoals();
+    this.loadGoals();;
+    // Hide the animation after 5 seconds and show the workflow content
+    setTimeout(() => {
+      this.showAnimation = false;
+    }, 6000);
   }
+
 
   loadGoals(): void {
     this.goalService.getAllGoals().subscribe(data => {
